bcc2849884f17f88ac24dec673000f9f884aa031
Geralt0714 <dz1108@nyu.edu>
2017-11-25 19:17:45 -0500

    Update

--- FeesRelatedMining/green_map.sh
+++ FeesRelatedMining/green_map.sh
@@ -1,3 +1,3 @@
 . /etc/profile.d/modules.sh
 module load python/gnu/2.7.11
-python sample/reduce.py
+python ./green_map.py
--- FeesRelatedMining/green_reduce.sh
+++ FeesRelatedMining/green_reduce.sh
@@ -1,3 +1,3 @@
 . /etc/profile.d/modules.sh
 module load python/gnu/2.7.11
-python sample/map.py
+python ./green_reduce.py
