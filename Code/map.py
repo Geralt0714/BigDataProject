f6d2f2e12735f5332b650406a71458ea74d14ca0
Geralt0714 <dz1108@nyu.edu>
2017-12-14 16:36:39 -0500

    Update map

--- Code/map.py
+++ Code/map.py
@@ -76,27 +76,47 @@ for line in sys.stdin:
 		continue
 	if (line[0]=="V"or line[0]=='v'):
 		continue
-	line = line.split(",")
-	## prepare data
-			year_P,month_P,date_P = int(line[1].split(" ")[0].split("-"))
-			hour_P,minu_P,seco_P = int(line[1].split(" ")[1].split(':'))
 
-			year_D,month_D,date_D = int(line[2].split(" ")[0].split("-"))
-			hour_D,minu_D,seco_D = int(line[2].split(" ")[1].split(':'))
+
+	line = line.split(",")
+	if float(line[9])==0:
+		continue
+	## prepare data
+	year_P,month_P,date_P = line[1].split(" ")[0].split("-")
+	year_P = int(year_P)
+	month_P = int(month_P)
+	date_P=int(date_P)
+	hour_P,minu_P,seco_P = line[1].split(" ")[1].split(':')
+	hour_P = int(hour_P)
+	minu_P = int(minu_P)
+	seco_P = int(seco_P)
+
+	year_D,month_D,date_D = line[2].split(" ")[0].split("-")
+	year_D = int(year_D)
+	month_D = int(month_D)
+	date_D = int(date_D)
+	hour_D,minu_D,seco_D = line[2].split(" ")[1].split(':')
+	hour_D = int(hour_D)
+	minu_D = int(minu_D)
+	seco_D = int(seco_D)
+
 
 	key=0
 	value = 0
 	if float(line[13])<15:
 		key=1.1
 		value = 1
-	if (float(line[13])>=15 and float(line[13])<35):
+	if (float(line[13])>=15 and float(line[13])<40):
 		key = 1.2
 		value = 1
-	if (float(line[13])>=35 and float(line[13])<80):
+	if (float(line[13])>=40 and float(line[13])<70):
 		key = 1.3
 		value = 1
-	if float(line[13])>=80:
+	if (float(line[13])>=70 and float(line[13])<100):
 		key = 1.4
+		value = 1
+	if float(line[13])>=100 :
+		key = 1.5
 		value = 1
 
 	print("%s,%s" %(key,value));
@@ -106,18 +126,21 @@ for line in sys.stdin:
 
 	
 
-	if (float(line[11])==0):
-		key = 2.1
-		value = 1
+	if (line[14] != 'CRD'):
+		print('2.1,1')
 	else:
-		key = 2.2
-		value = 1
-	print("%s,%s" %(key,value));
+		print('2.2,1')
+		if (float(line[11])==0):
+			print('2.3,1')
+		else:
+			percent = float(11)/float(9)
+			if percent >0:
+				print ('2.5,1')
+			print('2.4,%s' %(percent))
+	
 
 	if (check_midtown(float(line[3]),float(line[4]))):
-		temp = line[1].split(" ")[1]
-		temp = int(temp.split(':')[0])
-		print("3-%s,1" %(temp))
+		print("3-%s,1" %(hour_P))
 		print("4.1,1")
 	else:
 		print("4.2,1")
